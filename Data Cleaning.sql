389b257e561bda24bf084534268b4b2b45735173
pbansfore <pbf334@gmail.com>
2025-03-24 15:01:40 +0530

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -2,23 +2,28 @@ create database coffee_shop_sales_db;
 select *from coffee_shop_sales;
 desc coffee_shop_sales;
 
--- DATA CLEANING--------------------------------------------
+---------------------------------------------------------DATA_CLEANING--------------------------------------------------------------------------------------
 
--- Changing the data type of transaction_date from text to date 
+-- Changing the data type of transaction_date from text to date------------------------------------------------- 
+
 UPDATE coffee_shop_sales
 SET transaction_date= STR_TO_DATE(transaction_date,'%d-%m-%Y');
 
 ALTER TABLE coffee_shop_sales
 MODIFY COLUMN transaction_date DATE;
 
--- Changing the data type of transaction_time from text to time
+
+-- Changing the data type of transaction_time from text to time-------------------------------------------------
+
 UPDATE coffee_shop_sales
 SET transaction_time= STR_TO_DATE(transaction_time,'%H:%i:%s');
 
 ALTER TABLE coffee_shop_sales
 MODIFY COLUMN transaction_time TIME;
 
--- Change column name and set data type
+
+-- Change column name and set data type--------------------------------------------------------------------------
+
 ALTER TABLE coffee_shop_sales
 CHANGE COLUMN ï»¿transaction_id transaction_id INT;
 
