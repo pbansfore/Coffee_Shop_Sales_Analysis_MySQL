7ff403e674b460673b768d7b45633aa7e52ab28d
pbansfore <pbf334@gmail.com>
2025-03-24 15:31:07 +0530

    Update Exploratory Data Analysis.sql

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -6,7 +6,7 @@ DESC coffee_shop_sales;
 
 ---------------------------------------------------------DATA_CLEANING--------------------------------------------------------------------------------------
 
--- Changing the data type of transaction_date from text to date------------------------------------------------- 
+1)Changing the data type of transaction_date from text to date------------------------------------------------- 
 
 UPDATE coffee_shop_sales
 SET transaction_date= STR_TO_DATE(transaction_date,'%d-%m-%Y');
@@ -15,7 +15,7 @@ ALTER TABLE coffee_shop_sales
 MODIFY COLUMN transaction_date DATE;
 
 
--- Changing the data type of transaction_time from text to time-------------------------------------------------
+2)Changing the data type of transaction_time from text to time-------------------------------------------------
 
 UPDATE coffee_shop_sales
 SET transaction_time= STR_TO_DATE(transaction_time,'%H:%i:%s');
@@ -24,7 +24,7 @@ ALTER TABLE coffee_shop_sales
 MODIFY COLUMN transaction_time TIME;
 
 
--- Change column name and set data type--------------------------------------------------------------------------
+3)Change column name and set data type--------------------------------------------------------------------------
 
 ALTER TABLE coffee_shop_sales
 CHANGE COLUMN ï»¿transaction_id transaction_id INT;
